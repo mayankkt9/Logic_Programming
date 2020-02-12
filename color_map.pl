093ef4c430646a5df78a3a59823ba8966ae53452
Mayank Kataruka <mayank.kt9@gmail.com>
2020-02-11 20:22:23 -0700

    Assignment_1 problems upload

---
+++ color_map.pl
@@ -0,0 +1,36 @@
+vertex(1).
+vertex(2).
+vertex(3).
+vertex(4).
+vertex(5).
+vertex(6).
+
+
+edge(1,2).
+edge(1,3).
+edge(1,6).
+edge(1,4).
+edge(2,5).
+edge(2,3).
+edge(3,4).
+edge(6,4).
+edge(5,4).
+edge(5,3).
+edge(6,3).
+
+connection(X,Y) :-
+	edge(X,Y);edge(Y,X). 
+
+color(red).
+color(blue).
+color(green).
+color(yellow).
+
+
+do_coloring(_) :-
+	vertex(X),
+	write(X).
+
+color_map(L) :-
+	do_coloring(L).
+	
---
+++ full_words.pl
@@ -0,0 +1,24 @@
+to_words(0,zero).
+to_words(1,one).
+to_words(2,two).
+to_words(3,three).
+to_words(4,four).
+to_words(5,five).
+to_words(6,six).
+to_words(7,seven).
+to_words(8,eight).
+to_words(9,nine).
+
+full_words(N) :-
+	(N=:=0->write(zero);convert_to_words(N,0)).
+
+convert_to_words(0,_).
+convert_to_words(N,Len) :-
+	N>0,
+	N1 is N mod 10,
+	N2 is div(N,10),
+	L1 is Len + 1,
+	convert_to_words(N2,L1),
+	to_words(N1,Words),
+	write(Words),
+	(L1>1->write('-')).
