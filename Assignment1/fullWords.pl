7057e27391bbb7974502a2dcca182c1f44048710
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:28:39 -0700

    Assignment_1

--- Assignment1/fullWords.pl
+++ Assignment1/fullWords.pl
@@ -1,3 +1,5 @@
+
+%% Database to convert each digit to words
 to_words(0,zero).
 to_words(1,one).
 to_words(2,two).
@@ -9,10 +11,14 @@ to_words(7,seven).
 to_words(8,eight).
 to_words(9,nine).
 
+%% Predicate handling only zero case
 full_words(N) :-
 	(N=:=0->write(zero);convert_to_words(N,0)).
 
+%% If number is zero stopping condition
 convert_to_words(0,_).
+
+%% Predicate to extract each digit and print it in words
 convert_to_words(N,Len) :-
 	N > 0,
 	N1 is N mod 10,
