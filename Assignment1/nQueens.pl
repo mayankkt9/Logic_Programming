3ef352628df2c9980c21b96ce4e1ebf631ac90f0
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:29:08 -0700

    Assignment_1

--- Assignment1/nQueens.pl
+++ Assignment1/nQueens.pl
@@ -1,7 +1,13 @@
+
+%% Wrapper Predicate that generate row and then calls queens predicate
 queens(N,Result) :-
 	make_all_row(1,N,Row),
 	queens([],Result,Row).
 
+%% This predicate select rows to put queens at, if it finds the safe position 
+%% it goes to the next column and try the same thing again, if at anytime queens 
+%% attack , it tries different row, and if all column are exhausted it backtracks 
+%% to previous column and tries different rows.
 queens(ResultSoFar,Result,AllRowList) :-
 	select(PickedRow,AllRowList,NotPicked),
 	not(not_safe(PickedRow,ResultSoFar)),
@@ -9,18 +15,20 @@ queens(ResultSoFar,Result,AllRowList) :-
 
 queens(Result,Result,[]).
 
+%% This predicate checks if the current queen placed is not attacked 
+%% by the previous queens that are already placed. Counter is starting from 1 because
+%% the safe queen list made is in reverse.
 not_safe(Row,AllRow) :-
 	not_safe(Row,1,AllRow).
-
-not_safe(Row,Counter,[H|_]) :-
-	Row is H + Counter;
+not_safe(Row,Counter,[H|_]) :- 
+	Row is H + Counter.
+not_safe(Row,Counter,[H|_]) :- 
 	Row is H - Counter.
-
 not_safe(Row,Counter,[_|T]) :-
 	C1 is Counter+1,
 	not_safe(Row,C1,T).
 
-
+%% This predicate is used to generate row lists
 make_all_row(End,End,[End]).
 make_all_row(Start,End,[Start|Tail]) :-
 	Start < End,
