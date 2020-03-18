45f001234d88b42e237361b690cfef55f4e41d94
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:27:44 -0700

    Assignment_1

--- Assignment1/combination.pl
+++ Assignment1/combination.pl
@@ -1,6 +1,10 @@
 
-
+%% Stopping condition when N is 0
 combination(0,_,[]).
+
+%% Combination predicate that selects each number from the 
+%% list such that on the next call previous to the selected 
+%% number is not available
 combination(N,List,[H|T]) :-
 	N>0,
 	select_after(H,List,Res),
@@ -8,6 +12,7 @@ combination(N,List,[H|T]) :-
 	combination(N1,Res,T).
 
 
+%% When head matches return tail
 select_after(H,[H|T],T).
 %% Dont append number previous to X
 select_after(X,[_|T],Result) :-
