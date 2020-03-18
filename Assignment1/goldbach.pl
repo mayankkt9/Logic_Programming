c0119603232852c224f0764b5db412a6538b3e75
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:28:50 -0700

    Assignment_1

--- Assignment1/goldbach.pl
+++ Assignment1/goldbach.pl
@@ -1,3 +1,4 @@
+%% Checking which N is prime or not
 is_prime(2).
 is_prime(3).
 is_prime(N) :-
@@ -12,7 +13,7 @@ factor(N,Counter) :-
 	C1 is Counter+2,
 	factor(N,C1).
 
-
+%% Given current number, it calculates next prime 
 next_prime(CurrentPrime,NextPrime) :-
 	Val is CurrentPrime + 2,
 	is_prime(Val),
@@ -23,11 +24,15 @@ next_prime(Current,CurrentNext) :-
 	not(is_prime(N)),
 	next_prime(N,CurrentNext).
 
+%% Wrapper predicate that just handle some edge cases
 goldbach(Input,Result) :-
 	Input>3,
 	Input mod 2 =:=0,
 	(Input =:= 4 -> Result=[2,2];goldbach(Input,Result,3)).
 
+%% Given first number it calculates the second by subtracting 
+%% the first from the input and checks whether it is prime, 
+%% if it is not prime it calculates the next prime.
 goldbach(Input,Result,First) :-
 	Second is Input - First,
 	First=<Second,
