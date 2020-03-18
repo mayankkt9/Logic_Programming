6dfcd4710e4566e0bf7ab521a7c347b8b5cad4f4
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:29:34 -0700

    Assignment_1

--- Assignment1/hanoi.pl
+++ Assignment1/hanoi.pl
@@ -1,16 +1,17 @@
-%% ?- hanoi(3,a,c,b).
 
-hanoi(1,A,B,_) :-
+%% Printing Disk Moved From -> To 
+hanoi(1,From,To,_) :-
 	write('Move '),
-	write(A),
+	write(From),
 	write(' '),
 	write('to '),
-	write(B),
+	write(To),
 	nl.
 
-hanoi(N,A,C,B) :-
+%% Predicate to calculate way to transfer disk from source to destination using intermediate peg
+hanoi(N,Source,Destination,Intermediate) :-
 	N>1,
 	N1 is N-1,
-	hanoi(N1,A,B,C),
-	hanoi(1,A,C,_),
-	hanoi(N1,B,C,A).
+	hanoi(N1,Source,Intermediate,Destination),
+	hanoi(1,Source,Destination,_), %% Method specifically to call print Method
+	hanoi(N1,Intermediate,Destination,Source).
