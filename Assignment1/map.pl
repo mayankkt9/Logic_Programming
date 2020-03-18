8c75a0627b1690f132bfb854537f206eb2482bbe
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-17 23:29:00 -0700

    Assignment_1

--- Assignment1/map.pl
+++ Assignment1/map.pl
@@ -1,3 +1,4 @@
+%% Vertex Database
 vertex(1).
 vertex(2).
 vertex(3).
@@ -5,7 +6,7 @@ vertex(4).
 vertex(5).
 vertex(6).
 
-
+%% Edge Database
 edge(1,2).
 edge(1,3).
 edge(1,6).
@@ -18,18 +19,23 @@ edge(5,4).
 edge(5,3).
 edge(6,3).
 
+%% Undirected Edge statisfies this rule 
 connection(X,Y) :-
 	edge(X,Y);edge(Y,X). 
 
+%% Color Database
 color(red).
 color(blue).
 color(green).
 color(yellow).
 
+%% Wrapper predicate
 color_map(L) :-
 	generate_vertex_list(VertexList),
 	color_map(L,[],VertexList).
 
+%% This predicate does all the work, select vertex one by one 
+%% and applies all color to it if it is safe
 color_map(Result,ResultSoFar,VertexList) :-
 	select_after(V,VertexList,RemainList),
 	not(member_list(V,ResultSoFar)),
@@ -37,18 +43,22 @@ color_map(Result,ResultSoFar,VertexList) :-
 	is_safe(V,C,ResultSoFar),
 	append(ResultSoFar,[[V,C]],ResultTemp),
 	color_map(Result,ResultTemp,RemainList).
+
+%% This predicate is the stopping condition when the length 
+%% of the solution is equal to the number of vertices
 color_map(Result,ResultSoFar,_) :-
-	aggregate_all(count, vertex(X), Count),
+	aggregate_all(count, vertex(_), Count),
 	length(ResultSoFar,CurrentCount),
 	Count == CurrentCount,
 	Result = ResultSoFar.
 
+%% Predicate to select if the vertex is not in the visited list
 member_list(V,[[V,_]|_]).
 member_list(V,[[_,_]|Tail]) :-
 	member_list(V,Tail).
 
+%% Checking whether giving colors attacks or not
 is_safe(_,_,[]).
-is_safe(_,_,[[]]).
 is_safe(Vertex,Color,[[V,C]|Tail]) :-
 	connection(Vertex,V),
 	Color \= C,
